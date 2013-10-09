b6d03e17b8676753698e0cb082bd8504e53549a3
m1sogi <dev@misogi.info>
2013-10-09 17:37:09 +0900

    edit to end of Number 2

--- obnekyou/basics2.hs
+++ obnekyou/basics2.hs
@@ -1,5 +1,13 @@
 sq x = x * x
 
-pyth a b = a * a + b * b
+pyth' a b = a * a + b * b
+pyth (a, b) = a * a + b * b
+len vec = sqrt (pyth vec)
+lenSeg ((x, y), (x', y')) = sqrt $ pyth (x' - x, y' - y)
+getThePoint(_, pt, _) = pt
+flop (a, b) = (b, a)
+makeSegment p1 p2 = (p1, p2)
+center (x, y) (x2, y2) = ((x + x2) / 2 , (y + y2) /2)
 
-main = print $ sq 12
+main = print $
+    center (1,2) (3,4)
