ece4a46ca76b460178fbce0f55b4f523c970dcbe
m1sogi <ryudo@misogi.info>
2015-12-18 17:21:03 +0900

    solve Triangle

--- sugoi/baby.hs
+++ sugoi/baby.hs
@@ -2,4 +2,5 @@ doubleMe x = x + x
 doubleUs x y = doubleMe x + doubleMe y
 doubleSmallNumber x = if x > 100
                         then x
-                        else x * 2 
+                        else x * 2
+                        
--- sugoi/list.hs
+++ sugoi/list.hs
@@ -1,3 +1,4 @@
 boomBangs xs = [if x < 10 then "BOOM" else "BANG" | x <- xs, odd x ]
 length' xs = sum [1| _ <- xs]
 removeNonUppercase st = [c | c <- st, c `elem` ['A'..'Z']]
+let rightTriangles = [(a,b,c)|c <- [1..10], a <- [1..c], b <- [1..a], a+b+c == 24, a^2 + b^2 == c^2]
