3ae4f1be620e2619a2e289f1457f5a5b6bf59510
misogi <dev@misogi.info>
2015-12-19 12:45:52 +0900

    map method

--- sugoi/curry.hs
+++ sugoi/curry.hs
@@ -18,3 +18,10 @@ zipWith' :: (a -> b -> c) -> [a] -> [b] -> [c]
 zipWith' _ [] _ = []
 zipWith' _ _ [] = []
 zipWith' f (x:xs) (y:ys) = f x y : zipWith' f xs ys
+
+flip' :: (a -> b -> c) -> b -> a -> c
+flip' f y x = f x y
+
+map' :: (a -> b) -> [a] -> [b]
+map' _ [] = []
+map' f (x:xs) = f x : map f xs
