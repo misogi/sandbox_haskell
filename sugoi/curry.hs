11a1c84ab628e85f176cbaea6218ad846738bc58
misogi <dev@misogi.info>
2015-12-19 10:48:35 +0900

    部分適用

--- sugoi/curry.hs
+++ sugoi/curry.hs
@@ -3,3 +3,9 @@ multThree x y z = x * y * z
 
 compareWithHundred :: Int -> Ordering
 compareWithHundred = compare 100
+
+divideByTen :: (Floating a) => a -> a
+divideByTen = (/10)
+
+isUpperAlphanum :: Char -> Bool
+isUpperAlphanum = (`elem` ['A'..'Z'])
