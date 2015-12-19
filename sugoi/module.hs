c2816dc06650a931888dc2d5c68186b52762be81
misogi <dev@misogi.info>
2015-12-19 20:46:36 +0900

    any + isPrefixOf

--- sugoi/module.hs
+++ sugoi/module.hs
@@ -5,3 +5,6 @@ numUniques = length . nub
 
 wordNums :: String -> [(String, Int)]
 wordNums = map (\ws -> (head ws, length ws)) . group . sort . words
+
+isIn :: (Eq a) => [a] -> [a] -> Bool
+needle `isIn` haystack = any (needle `isPrefixOf`) (tails haystack)
