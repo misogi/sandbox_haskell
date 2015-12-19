4af2b9579813c5bc93fa160045b29dc4b4ba781d
misogi <dev@misogi.info>
2015-12-19 12:49:50 +0900

    filter method

--- sugoi/curry.hs
+++ sugoi/curry.hs
@@ -25,3 +25,9 @@ flip' f y x = f x y
 map' :: (a -> b) -> [a] -> [b]
 map' _ [] = []
 map' f (x:xs) = f x : map f xs
+
+filter' :: (a -> Bool) -> [a] -> [a]
+filter' _ [] = []
+filter' p (x:xs)
+  | p x = x : filter' p xs
+  | otherwise = filter' p xs
