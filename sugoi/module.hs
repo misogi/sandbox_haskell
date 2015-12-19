4658279a18cf905bfdc2b2c893b300eda52b512f
misogi <dev@misogi.info>
2015-12-19 20:53:23 +0900

    Data.Char

--- sugoi/module.hs
+++ sugoi/module.hs
@@ -1,4 +1,5 @@
 import Data.List
+import Data.Char
 
 numUniques :: (Eq a) => [a] -> Int
 numUniques = length . nub
@@ -8,3 +9,9 @@ wordNums = map (\ws -> (head ws, length ws)) . group . sort . words
 
 isIn :: (Eq a) => [a] -> [a] -> Bool
 needle `isIn` haystack = any (needle `isPrefixOf`) (tails haystack)
+
+encode :: Int -> String -> String
+encode offset msg = map (\c -> chr $ ord c + offset) msg
+
+decode :: Int -> String -> String
+decode shift msg = encode (negate shift) msg
