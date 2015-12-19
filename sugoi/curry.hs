e3aa64343c4ee9134b05cd2314fadb3557b0e50d
misogi <dev@misogi.info>
2015-12-19 16:49:54 +0900

    標準関数をたたみ込みで

--- sugoi/curry.hs
+++ sugoi/curry.hs
@@ -77,3 +77,18 @@ mapl f xs = foldr (\x acc -> f x : acc) [] xs
 
 mapr :: (a -> b) -> [a] -> [b]
 mapr f xs = foldl (\acc x -> acc ++ [f x]) [] xs
+
+maximum' :: (Ord a) => [a] -> a
+maximum' = foldl1 max
+
+reverse' :: [a] -> [a]
+reverse' = foldl (\acc x -> x : acc) []
+
+reverse'' :: [a] -> [a]
+reverse'' = foldl (flip (:)) []
+
+product' :: (Num a) => [a] -> a
+product' = foldl (*) 1
+
+filter''  :: (a -> Bool) -> [a] -> [a]
+filter'' p = foldr (\x acc -> if p x then x : acc else acc) []
