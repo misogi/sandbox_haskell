d939c663eebb28179e619c9bf01b038acb31d35a
misogi <dev@misogi.info>
2015-12-19 21:28:49 +0900

    foldr version

--- sugoi/module.hs
+++ sugoi/module.hs
@@ -33,3 +33,7 @@ findKey key [] = Nothing
 findKey key ((k,v):xs)
   | key == k = Just v
   | otherwise = findKey key xs
+
+findKey' :: (Eq k) => k -> [(k, v)] -> Maybe v
+findKey' key xs = foldr
+  (\(k, v) acc -> if key == k then Just v else acc) Nothing xs
