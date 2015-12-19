0b48219807ad2cbbd770501d925d6b9ee1eaa85c
misogi <dev@misogi.info>
2015-12-19 11:07:01 +0900

    function as argument

--- sugoi/curry.hs
+++ sugoi/curry.hs
@@ -12,3 +12,9 @@ isUpperAlphanum = (`elem` ['A'..'Z'])
 
 applyTwice :: (a -> a) -> a -> a
 applyTwice f x = f (f x)
+
+-- zipWith max [1,2,3] [4,0,6]
+zipWith' :: (a -> b -> c) -> [a] -> [b] -> [c]
+zipWith' _ [] _ = []
+zipWith' _ _ [] = []
+zipWith' f (x:xs) (y:ys) = f x y : zipWith' f xs ys
