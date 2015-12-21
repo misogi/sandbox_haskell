1002680a6d748fe51c2a95cc966b584d25f77a93
misogi <dev@misogi.info>
2015-12-21 10:40:52 +0900

    export as module

--- sugoi/07type.hs
+++ sugoi/07type.hs
@@ -1,3 +1,12 @@
+module Shapes
+( Point(..)
+, Shape(..)
+, area
+, nudge
+, baseCircle
+, baseRect
+) where
+
 data Point = Point Float Float deriving (Show)
 data Shape = Circle Point Float |
   Rectangle Point Point
@@ -12,3 +21,9 @@ nudge :: Shape -> Float -> Float -> Shape
 nudge (Circle (Point x y) r) a b = Circle (Point (x+a) (x+b)) r
 nudge (Rectangle (Point x1 y1) (Point x2 y2)) a b =
   Rectangle (Point (x1+a) (y1+b)) (Point (x2 +a) (y2+b))
+
+baseCircle :: Float -> Shape
+baseCircle r = Circle (Point 0 0) r
+
+baseRect :: Float -> Float -> Shape
+baseRect width height = Rectangle (Point 0 0) (Point width height)
