ff2c9301e315213c08a891cd8af69a586f7feb67
misogi <dev@misogi.info>
2015-12-21 18:59:43 +0900

    functor

--- sugoi/yesno.hs
+++ sugoi/yesno.hs
@@ -31,3 +31,7 @@ class Functor' f where
 instance Functor' Maybe where
   fmap' f (Just x) = Just (f x)
   fmap' f Nothing = Nothing
+
+instance Functor' (Either a) where
+  fmap' f (Right x) = Right (f x)
+  fmap' f (Left x) = Left x
