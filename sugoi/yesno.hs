240681871bec11889b59b6be8df96cd8cc530637
misogi <dev@misogi.info>
2015-12-21 17:54:15 +0900

    yesno class

--- sugoi/yesno.hs
+++ sugoi/yesno.hs
@@ -1,3 +1,18 @@
 -- 7.9
 class YesNo a where
   yesno :: a -> Bool
+
+instance YesNo Int where
+  yesno 0 = False
+  yesno _ = True
+
+instance YesNo Bool where
+  yesno = id
+
+instance YesNo [a] where
+  yesno [] = False
+  yesno _ = True
+
+instance YesNo (Maybe a) where
+  yesno (Just _) = True
+  yesno Nothing = False
