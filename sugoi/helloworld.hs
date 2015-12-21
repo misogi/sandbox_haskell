ed75c4debccaca1f4ccd46d22dd36ece8f822eaf
misogi <dev@misogi.info>
2015-12-21 20:09:08 +0900

    let and do

--- sugoi/helloworld.hs
+++ sugoi/helloworld.hs
@@ -1,7 +1,17 @@
 -- 8.1
 -- main = putStrLn "hello, world"
+import Data.Char
 
+main :: IO ()
 main = do
-  putStrLn "Hello , whats your name"
-  name <- getLine
-  putStrLn ("Hey " ++ name ++ ",you rock!")
+  putStrLn "Hello , whats your first name"
+  firstName <- getLine
+  putStrLn "Whats your las name"
+  lastName <- getLine
+  let bigFirstName = map toUpper firstName
+      bigLastName = map toUpper lastName
+  putStrLn $ "Hey " ++ bigFirstName
+    ++ ",you rock!" ++ bigLastName
+    ++ ", How are you."
+
+-- nameTag = "Hello, my name is " ++ getLine
