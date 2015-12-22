564cb564b4fe68511b0cf7f77140f1b1e93b9c8d
misogi <dev@misogi.info>
2015-12-22 17:08:22 +0900

    writeFile

--- sugoi/baabaa.hs
+++ sugoi/baabaa.hs
@@ -1,6 +1,7 @@
 -- 9.2
 
 import System.IO
+import Data.Char
 
 main = do
   handle <- openFile "haiku.txt" ReadMode
@@ -9,4 +10,4 @@ main = do
   hClose handle
   -- readFile はハンドルを自動で閉じる
   cont2 <- readFile "haiku.txt"
-  putStr cont2
+  writeFile "haiku2.txt" (map toUpper cont2)
