98cff80d4bd7a4ce8ff2d271670b47c5c3a91a09
misogi <dev@misogi.info>
2015-12-23 13:51:48 +0900

    ルートを計算

--- sugoi/route.hs
+++ sugoi/route.hs
@@ -1,3 +1,5 @@
+import Data.List
+
 data Section = Section {getA :: Int, getB :: Int, getC :: Int}
   deriving (Show)
 
@@ -6,14 +8,27 @@ type Path = [(Label, Int)]
 
 type RoadSystem = [Section]
 
-heathrowToLondon :: RoadSystem
-heathrowToLondon = [ Section 5 10 30
-  , Section 5 90 20
-  , Section 40 2 25
-  , Section 10 8 0
-  ]
+main = do
+  contents <- getContents
+  let threes = groupsOf 3 (map read $ lines contents)
+      roadSystem = map (\[a,b,c] -> Section a b c) threes
+      path = optimalPath roadSystem
+      pathString = concat $ map (show . fst) path
+      pathTime = sum $ map snd path
+  putStrLn $ "The best path to take is: " ++ pathString
+  putStrLn $ "Time taken: " ++ show pathTime
 
--- optimalPath :: RoadSystem -> Path
+groupsOf :: Int -> [a] -> [[a]]
+groupsOf 0 _ = undefined
+groupsOf _ [] = []
+groupsOf n xs = take n xs : groupsOf n (drop n xs)
+
+optimalPath :: RoadSystem -> Path
+optimalPath roadSystem =
+  let (bestAPath, bestBPath) = foldl roadStep ([], []) roadSystem
+  in if sum (map snd bestAPath) <= sum (map snd bestBPath)
+    then reverse bestAPath
+    else reverse bestBPath
 
 roadStep :: (Path, Path) -> Section -> (Path, Path)
 roadStep (pathA, pathB) (Section a b c) =
