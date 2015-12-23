c5f31ad7675a5c9ee546331f31dd4c77c3dd3cd3
misogi <dev@misogi.info>
2015-12-23 09:30:37 +0900

    guess the number

--- sugoi/random_string.hs
+++ sugoi/random_string.hs
@@ -1,7 +1,20 @@
 -- 9.6\
 
 import System.Random
+import Control.Monad(when)
 
 main = do
   gen <- getStdGen
-  putStrLn $ take 20 (randomRs ('a', 'z') gen)
+  askForNumber gen
+
+askForNumber :: StdGen -> IO ()
+askForNumber gen = do
+  let (randNumber, newGen) = randomR (1,10) gen :: (Int, StdGen)
+  putStrLn "Which number in the range from 1 to 10 am I thinking of? :"
+  numberString <- getLine
+  when (not $ null numberString) $ do
+    let number = read numberString
+    if randNumber == number
+      then putStrLn "you are correct"
+      else putStrLn $ "sorry, it was " ++ show randNumber
+    askForNumber newGen
