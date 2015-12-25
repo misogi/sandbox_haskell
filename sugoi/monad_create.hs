fe3add7a6c524278a5097405cbd0b6aa8e7bc992
misogi <dev@misogi.info>
2015-12-25 15:47:00 +0900

    create Prob as Functor

---
+++ sugoi/monad_create.hs
@@ -0,0 +1,16 @@
+-- 14.8
+import Data.Ratio
+
+a = 1%4
+
+newtype Prob a = Prob { getProb :: [(a, Rational)]}
+  deriving Show
+
+instance Functor Prob where
+  fmap f (Prob xs) = Prob $ map (\(x, p) -> (f x, p)) xs
+
+thisSituation :: Prob (Prob Char)
+thisSituation = Prob
+  [(Prob [('a', 1%2), ('b', 1%2)], 1%4)
+  ,(Prob [('c', 1%2), ('d', 1%2)], 3%4)
+  ]
