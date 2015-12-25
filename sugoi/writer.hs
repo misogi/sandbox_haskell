dd9d74b31fb06947717e653560ddaa60f312bb11
m1sogi <ryudo@misogi.info>
2015-12-25 11:53:42 +0900

    gcd function with log

--- sugoi/writer.hs
+++ sugoi/writer.hs
@@ -32,3 +32,11 @@ multWithLog = do
   b <- logNumber 5
   tell ["Gonna multiply these two"]
   return (a*b)
+
+gcd' :: Int -> Int -> Writer [String] Int
+gcd' a b
+  | b == 0 = do
+    writer (a, ["Finished with " ++ show a])
+  | otherwise = do
+    tell [show a ++ " mod " ++ show b ++ " = " ++ show (a `mod` b)]
+    gcd' b (a `mod` b)
