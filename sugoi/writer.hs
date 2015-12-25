876bc82c344293d224f7c44c5174f628f4fc8f59
misogi <dev@misogi.info>
2015-12-25 12:14:40 +0900

    右結合か左結合か

--- sugoi/writer.hs
+++ sugoi/writer.hs
@@ -40,3 +40,39 @@ gcd' a b
   | otherwise = do
     tell [show a ++ " mod " ++ show b ++ " = " ++ show (a `mod` b)]
     gcd' b (a `mod` b)
+
+gcdReverse :: Int -> Int -> Writer (DiffList String) Int
+gcdReverse a b
+  | b == 0 = do
+    tell $ toDiffList ["Finished with " ++ show a]
+    return a
+  | otherwise = do
+    result <- gcdReverse b (a `mod` b)
+    tell $ toDiffList [show a ++ " mod " ++ show b ++ " = " ++ show (a `mod` b)]
+    return result
+
+newtype DiffList a = DiffList {getDiffList :: [a] -> [a]}
+
+toDiffList :: [a] -> DiffList a
+toDiffList xs = DiffList (xs++)
+
+fromDiffList :: DiffList a -> [a]
+fromDiffList (DiffList f) = f []
+
+instance Monoid (DiffList a) where
+  mempty = DiffList (\xs -> [] ++ xs)
+  (DiffList f) `mappend` (DiffList g) = DiffList (\xs -> f (g xs))
+
+finalCountDown :: Int -> Writer [String] ()
+finalCountDown 0 = do
+  tell ["0"]
+finalCountDown x = do
+  finalCountDown (x-1)
+  tell [show x]
+
+finalCountDown' :: Int -> Writer (DiffList String) ()
+finalCountDown' 0 = do
+  tell (toDiffList ["0"])
+finalCountDown' x = do
+  finalCountDown' (x-1)
+  tell (toDiffList [show x])
