7e9ca6eb3e24e8b38c0b8783efd28dfa1b4d3cda
m1sogi <ryudo@misogi.info>
2015-12-25 19:16:21 +0900

    tree を操作する

--- sugoi/tree.hs
+++ sugoi/tree.hs
@@ -25,3 +25,18 @@ freeTree =
         (Node 'C' Empty Empty)
       )
     )
+
+data Direction = L | R
+  deriving (Show)
+
+type Directions = [Direction]
+
+changeToP :: Directions -> Tree Char -> Tree Char
+changeToP (L:ds) (Node x l r) = Node x (changeToP ds l) r
+changeToP (R:ds) (Node x l r) = Node x l (changeToP ds r)
+changeToP [] (Node _ l r) = Node 'P' l r
+
+elemAt :: Directions -> Tree a -> a
+elemAt (L:ds) (Node _ l _) = elemAt ds l
+elemAt (R:ds) (Node _ _ r) = elemAt ds r
+elemAt [] (Node x _ _) = x
