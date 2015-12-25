654b902adedb92529a828da00676ce4de0d28e5c
m1sogi <ryudo@misogi.info>
2015-12-25 10:57:26 +0900

    apply log

--- sugoi/writer.hs
+++ sugoi/writer.hs
@@ -1,4 +1,9 @@
 -- 14.1
 
-isBigGang :: Int -> Bool
-isBigGang x = x > 9
+isBigGang :: Int -> (Bool, String)
+isBigGang x = (x > 9, "Compared to 9.")
+
+applyLog :: (a, String) -> (a -> (b, String)) -> (b, String)
+applyLog (x, log) f = let (y, newLog) = f x  in (y, log ++ newLog)
+
+aplg = ("tobin", "Got outlaw name.") `applyLog` (\x -> (length x, "applied Length."))
