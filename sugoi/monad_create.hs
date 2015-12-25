386559879d3bff3456a4196dcf192e2db588b65d
misogi <dev@misogi.info>
2015-12-25 16:39:44 +0900

    create Monad

--- sugoi/monad_create.hs
+++ sugoi/monad_create.hs
@@ -14,3 +14,18 @@ thisSituation = Prob
   [(Prob [('a', 1%2), ('b', 1%2)], 1%4)
   ,(Prob [('c', 1%2), ('d', 1%2)], 3%4)
   ]
+
+flatten :: Prob (Prob a) -> Prob a
+flatten (Prob xs) = Prob $ concat $ map multAll xs
+  where
+    multAll (Prob innerxs, p) =
+      map (\(x, r) -> (x, p*r)) innerxs
+
+instance Applicative Prob where
+  pure x = Prob [(x, 1%1)]
+  Prob [(f, _)] <*> Prob y = Prob (map (\(ys, yp) -> (f ys, yp))  y)
+
+instance Monad Prob where
+  return x = Prob [(x, 1%1)]
+  m >>= f = flatten (fmap f m)
+  fail _ = Prob []
