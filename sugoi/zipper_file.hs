fd811c72acd706dd9c6ea9902dec960d3ec5a068
misogi <dev@misogi.info>
2015-12-28 10:17:44 +0900

    move on file system

--- sugoi/zipper_file.hs
+++ sugoi/zipper_file.hs
@@ -1,3 +1,5 @@
+import Data.List(break)
+
 type Name = String
 type Data = String
 data FSItem = File Name Data | Folder Name [FSItem]
@@ -7,5 +9,33 @@ myDisk :: FSItem
 myDisk =
   Folder "root"
     [ File "goat_yeling_like_man.wmv" "baaaa"
-    , File "pope_time.avi" ""
+    , File "pope_time.avi" "god bless"
+    , Folder "pics"
+      [ File "ape_throwing_up.jpg" "bleargh"
+      , File "watermelon_smash.gif" "smash!"
+      , File "skull_man(scary).bmp" "Yikes!"]
     ]
+
+x -: f = f x
+
+data FSCrumb = FSCrumb Name [FSItem] [FSItem]
+  deriving (Show)
+type FSZipper = (FSItem, [FSCrumb])
+
+fsUp :: FSZipper -> FSZipper
+fsUp (item, FSCrumb name ls rs:bs) =
+  (Folder name (ls ++ [item] ++ rs), bs)
+
+fsTo :: Name -> FSZipper -> FSZipper
+fsTo name (Folder folderName items, bs) =
+  let (ls, item:rs) = break (nameIs name) items
+  in (item, FSCrumb folderName ls rs:bs)
+
+nameIs :: Name -> FSItem -> Bool
+nameIs name (Folder folderName _) = name == folderName
+nameIs name (File fileName _) = name == fileName
+
+newFocus =
+  (myDisk, []) -: fsTo "pics" -: fsTo "skull_man(scary).bmp"
+
+newFocus2 = newFocus -: fsUp -: fsTo "watermelon_smash.gif" 
